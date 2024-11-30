e7ef1af676e7b7096bd7f00a53f0dffaa8ddc8f8
Antibas <csd3740@csd.uoc.gr>
2024-11-30 16:23:00 +0200

    feat: :sparkles: Introduces ejs

--- app.js
+++ app.js
@@ -5,22 +5,28 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    console.log(`${req.method} / ${res.statusCode} ${res.statusMessage}`);
-    res.sendFile(path.join(__dirname, 'public', '/index.html'));
+    // console.log(`${req.method} / ${res.statusCode} ${res.statusMessage}`);
+    // res.sendFile(path.join(__dirname, 'public', '/index.html'));
+    res.render("index");
 });
 
 app.get("/send_email", (req, res) => {
-    console.log(`${req.method} /send_email ${res.statusCode} ${res.statusMessage}`);
-    res.sendFile(path.join(__dirname, 'public', '/send_email.html'));
+    // console.log(`${req.method} /send_email ${res.statusCode} ${res.statusMessage}`);
+    // res.sendFile(path.join(__dirname, 'public', '/send_email.html'));
+    res.render("send_email");
 });
 
 app.post("/send_email", (req, res) => {
     // TODO email send to me
-    console.log(`${req.method} /send_email ${res.statusCode} ${res.statusMessage}`);
-    res.sendFile(path.join(__dirname, 'public', '/index.html'));
+    // console.log(`${req.method} /send_email ${res.statusCode} ${res.statusMessage}`);
+    // res.sendFile(path.join(__dirname, 'public', '/index.html'));
+    res.render("index");
 });
 
 
